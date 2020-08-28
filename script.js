47e5a7b2cbcc3a757b4038744fe1d324e38c4918
keffri <keffri.w.neal@gmail.com>
2020-08-28 13:28:14 -0700

    basic function

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  let c = a + b;
+  return c;
+}
+
+function subtract(a, b) {
+  let c = a - b;
+  return c;
+}
+
+function multiply(a, b) {
+  let c = a * b;
+  return c;
+}
+
+function divide(a, b) {
+  let c = a / b;
+  return c;
+}
+
+function operate(a, b, op) {
+  let c = op(a, b);
+  return c;
+}
