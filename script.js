4826afa98ebcf967c95f1a25319326c90a1aa5e0
keffri <keffri.w.neal@gmail.com>
2020-09-22 17:50:53 -0700

    multiple decimals removed

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+/***** BUTTON ASSIGNMENTS *****/
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+// const previousOperandText = document.querySelector("[data-previous-operand]");
+const currentOperandText = document.querySelector("[data-current-operand]");
+let previousValue = "";
+let currentValue = "";
+let chosenOperator = undefined;
 
 /***** FUNCTIONS  *****/
 
@@ -27,3 +37,57 @@ function operate(a, b, op) {
   let c = op(a, b);
   return c;
 }
+
+/***** EVENT LISTENERS *****/
+
+numberButtons.forEach((numButton) => {
+  numButton.addEventListener("click", () => {
+    if (
+      currentOperandText.textContent.includes(".") &&
+      numButton.textContent === "."
+    ) {
+      currentOperandText.textContent += "";
+    } else {
+      currentOperandText.textContent += numButton.textContent;
+    }
+  });
+});
+
+operatorButtons.forEach((opButton) => {
+  opButton.addEventListener("click", () => {
+    previousValue = currentOperandText.textContent;
+    currentOperandText.textContent = "";
+    if (opButton.textContent === "+") {
+      chosenOperator = add;
+    } else if (opButton.textContent === "-") {
+      chosenOperator = subtract;
+    } else if (opButton.textContent === "*") {
+      chosenOperator = multiply;
+    } else if (opButton.textContent === "÷") {
+      chosenOperator = divide;
+    }
+  });
+});
+
+equalsButton.addEventListener("click", () => {
+  if (chosenOperator === divide && currentOperandText.textContent === "0") {
+    previousValue = "";
+    currentOperandText.textContent = "";
+    alert("You cannot divide by zero.");
+    return;
+  }
+  currentOperandText.textContent = chosenOperator(
+    parseInt(previousValue),
+    parseInt(currentOperandText.textContent)
+  );
+  previousValue = "";
+});
+
+clearButton.addEventListener("click", () => {
+  previousValue = "";
+  currentOperandText.textContent = "";
+});
+
+deleteButton.addEventListener("click", () => {
+  currentOperandText.textContent = currentOperandText.textContent.slice(0, -1);
+});
